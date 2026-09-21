de77c184cf657fe4190fa2316b562cb85abde788
Modotti <luimodotti@gmail.com>
2026-09-21 20:54:58 -0300

    chore: configure Umami website

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   supabasePublishableKey: 'sb_publishable_7vOyVELDyH7QEIBsqyfEwQ_Dj9sMZP8',
   analytics: {
     enabled: true,
-    umamiWebsiteId: '',
+    umamiWebsiteId: '1acfaa08-0dfe-4eab-8681-baa289ae2307',
     umamiHostUrl: 'https://cloud.umami.is',
   },
 } as const;
